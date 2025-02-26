5f67d75844c27d7cf26f93cf7e116dac5893d624
sukhpreetphp2 <sukh191105@gmail.com>
2025-02-26 12:56:35 +0530

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>ElectroHub</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="header">
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="Image/electrohub-logo.png" width="125px">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Account</a></li>
+                </ul>
+            </nav>
+            <img src="Image/shopping-cart.png" width="30px" height="30px">
+        </div>
+        <div class="row">
+            <div class="col-2">
+                <h1>Why ElectroHub?</h1>
+                <p>We have a great amount of experience in building Gaming PC, Editing PC & Workstation PC for our clients. <br>We build with love and we know what your PC means to you. Just give us a chance to serve you and get your Dream Gaming PC Build or Editing PC Build from us.<br> So if you’re looking to buy PC online then you’re at the right place. For best Online PC Build services and prices shop ElectroHub!</p>
+                <a href="" class="btn">Explore Now &#8594</a>
+            </div>
+            <div class="col-2">
+                <img src="Image/pngtree-vibrant-pc-case-showcase-png-image_11081161.png">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="categories">
+    <div class="small-container">
+        <h1>Categories:-</h1>
+        <div class="row">
+            <div class="col-3">
+                <img src="Image/intel.png">
+            </div>
+            <div class="col-3">
+                <img src="Image/Amd.jpg">
+            </div>
+            <div class="col-3">
+                <img src="Image/8221cd404d0605500e5321151b8589ff.png">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="small-container">
+    <h2 class="title">Featured Products</h2>
+    <div class="row">
+        <div class="col-4">
+            <img src="Image/Memória G_Skill Trident Z RGB, 32GB (2x16GB), 3200MHz, DDR4, CL16 - F4-3200C16D-32GTZR.jpg">
+            <h4>Memória G_Skill Trident Z RGB, 32GB (2x16GB), 3200MHz, DDR4, CL16 - F4-3200C16D-32GTZR</h4>
+            <p>RS.5370</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/i3 cpu.jpg">
+            <h4>Intel Core i3-8350K 4 GHz LGA 1151 Socket 4 Cores Desktop, Laptop Processor (Black)</h4>
+            <p>RS.13000</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/i5.jpg">
+            <h4>Intel i5-12400F 4.4 GHz Upto 4.4 GHz LGA1700 Socket 6 Cores 12 Threads Desktop Processor (Blue)</h4>
+            <p>RS.18000</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/i7 7700.jpg">
+            <h4>Intel Core i7 7700 3.6 GHz LGA 1151 Socket 4 Cores Desktop Processor (GREY)</h4>
+            <p>RS.40000</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/i7.jpeg">
+            <h4>Intel Core i7 7700 3.6 GHz LGA 1151 Socket 4 Cores Desktop Processor (BLUE)</h4>
+            <p>RS.35699</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/RTX 3090.jpg">
+            <h4>MSI NVIDIA RTX 3090 GAMING X TRIO 24G 24 GB GDDR6X Graphics Card (Black)</h4>
+            <p>RS.105000</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/RTX 730.jpg">
+            <h4>MSI GeForce GT 730 ddr3_sdram pci_e_x4 4GB Graphics Card - 4GB DDR3, PCI Express 2.0, DVI-D x1, D-SUB x1, HDMI 1.4a x1 (N730K-4GD3/OCV1)</h4>
+            <p>RS.6212</p>
+        </div>
+        <div class="col-4">
+            <img src="Image/RTX 3090.jpg">
+            <h4>INNO3D Nvidia Geforce RTX 3090 6Gb Twin X2 Gddr6 96-Bit Pci-E 4.0 Integrated Gaming Graphics Card - N30502-06D6-1711Va60</h4>
+            <p>RS.16999</p>
+        </div>
+    </div>
+</div>
+
+<div class="offer">
+    <div class="small-container">
+        <div class="row">
+            <div class="col-2">
+                <img src="Image/Corsair-H150-RGB-360mm-Liquid-CPU-Cooler-1.png" class="offer-img">
+            </div>
+            <div class="col-2">
+                <p>Exclusively Available on Electrohub</p>
+                <h1>AIO LIQUID COOLER</h1>
+                <small>Features: Copper base with 90 skived fins and more micro water channels to get more conductive surface area, and speed up the cooling liquid flow. Universal Compatibility For Mainstream Platform Intel: LGA1700/775/115X/1366/2066/2011/1200</small>
+                <a href="" class="btn">BUY NOW &#8594</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
